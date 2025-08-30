3e939628a952ad930b2f8dec244a446aa8ea76a1
Tushita Agrawal <tushitaagrawalbca@gmail.com>
2025-08-30 21:40:37 +0530

    my another commit

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class Config:
     DB_PORT = 3306
     DB_USER = 'root'
     DB_PASSWORD = 'Admin&123'
-    DB_NAME = 'complaint_db'
-    SECRET_KEY = 'my_flask_secret_key_123'
+    DB_NAME = 'complaints_db'
+    SECRET_KEY = 'my_secret_key'
